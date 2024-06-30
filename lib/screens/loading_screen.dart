dc8e59369014d66c3cd3ffebbd6e6c657c7257ab
NEEL PRAJAPATI <83747762+catalog2003@users.noreply.github.com>
2024-06-30 12:51:33 +0530

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToSetupScreen();
+  }
+
+  _navigateToSetupScreen() async {
+    await Future.delayed(Duration(seconds: 2), () {});
+    Navigator.pushReplacementNamed(context, '/setup');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
